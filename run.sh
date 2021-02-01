8ca372c0ad57174cd383ec0460f661b5039c6acf
Chan Ger Hean <gerhean@live.com>
2021-02-01 13:42:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs --repos https://github.com/reposense/RepoSense.git https://github.com/gerhean/iwonbyalot.git
+java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git https://github.com/gerhean/iwonbyalot.git
